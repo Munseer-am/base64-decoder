baf692bc7ab326b462df44d4906912f2aa6342c0
Munseer-am <82161602+Munseer-am@users.noreply.github.com>
2021-10-15 10:10:51 +0530

    Add files via upload

---
+++ base64_decode.py
@@ -0,0 +1,18 @@
+import base64 
+
+  
+code = input('Enter base64 code: ')
+  
+base64_string = code
+
+base64_bytes = base64_string.encode("ascii") 
+
+  
+
+sample_string_bytes = base64.b64decode(base64_bytes) 
+
+sample_string = sample_string_bytes.decode("ascii") 
+
+  
+
+print(f"Decoded string: {sample_string}") 
